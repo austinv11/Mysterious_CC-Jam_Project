6ea9d6d0f95d01e8c0d6482630fc6a3f766a1afd
austinv11 <austinv11@gmail.com>
2014-02-08 12:49:59 -0500

    oops again

--- menu.lua
+++ menu.lua
@@ -101,6 +101,7 @@ local function selection()
 			print(" ")
 			mc.center("[Quit]")
 			end
+		insternal1()
 		--local mousef = internal1()
 		--local keyf = internal2()
 		--parallel.waitForAny(mousef, keyf)

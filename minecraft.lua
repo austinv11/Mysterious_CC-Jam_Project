876789e56d1a04830f991ac0f95bba84b9fcfda5
austinv11 <austinv11@gmail.com>
2014-02-08 19:06:06 -0500

    bugfix

--- minecraft.lua
+++ minecraft.lua
@@ -65,7 +65,7 @@ local function loadWorld(world)
 			ax = ax + 1
 			axx = tostring(ax)
 			ayy = tostring(ay)
-			local r = fs.open(",minecraft/world/"..name.."/"..axx..","..ayy, "r")
+			local r = fs.open(",minecraft/world/"..world.."/"..axx..","..ayy, "r")
 			local k = r.readAll()
 			r.close()
 			local coloring = mc.convertColor(k)

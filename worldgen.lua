404d91408e0140c899f6f00d51ba6542f19604fb
austinv11 <austinv11@gmail.com>
2014-02-09 20:11:39 -0500

    what's going on???

--- worldgen.lua
+++ worldgen.lua
@@ -101,8 +101,8 @@ for i = 1, y2 do
 				if block == 5 then
 					block = 3
 					end
-				local cBlock1 = mc.getBlockData(tArgs[1], (x-1), y)
-				local cBlock2 = mc.getBlockData(tArgs[1], x, (y+1))
+				Block1 = mc.getBlockData(tArgs[1], (x-1), y)
+				cBlock2 = mc.getBlockData(tArgs[1], x, (y+1))
 				if cBlock1 ~= blockids[block] and cBlock2 ~= blockids[block] then
 					block = math.random(5, 7)
 					if block == 5 then

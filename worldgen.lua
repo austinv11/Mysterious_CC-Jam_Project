f4d0195e65cfa06b02fcbf0693e699f9fea5f4fa
austinv11 <austinv11@gmail.com>
2014-02-11 20:23:29 -0500

    ok maybe one more try...

--- worldgen.lua
+++ worldgen.lua
@@ -115,14 +115,14 @@ for i = 1, y2 do
 							end
 						end
 					end
-				if blockids[block] == "blocks.dirt" then
-					if cBlock2 == "background.sky" then
-						block = 6
-						end
-					end
 				if blockids[block] == "blocks.grass" then
 					if cBlock2 ~= "background.sky" then
 						block = 7
+						end
+					end
+				if blockids[block] == "blocks.dirt" then
+					if cBlock2 == "background.sky" then
+						block = 6
 						end
 					end
 				docGen(x, y, block)
